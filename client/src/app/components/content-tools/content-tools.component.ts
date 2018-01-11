0dae7442c0040289639abbbea0be032b7c50a5c1
Nikola Iliev <nikola.iliev@sofyma.com>
2018-01-11 12:12:13 +0200

    setup jquery and range slider js

--- client/src/app/components/content-tools/content-tools.component.ts
+++ client/src/app/components/content-tools/content-tools.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * This component uses range slider which 
+ * you can find here: https://www.npmjs.com/package/ion-rangeslider.
+ * Demos : http://ionden.com/a/plugins/ion.rangeSlider/demo.html
+ */
+
+declare var $: any;
+
 @Component({
   selector: 'app-content-tools',
   templateUrl: './content-tools.component.html',
@@ -10,6 +18,9 @@ export class ContentToolsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $("#range").ionRangeSlider({
+      max: 640
+    });
   }
-
+ 
 }
