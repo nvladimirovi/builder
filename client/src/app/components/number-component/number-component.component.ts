3225543896b1dccb4061ef5bc038e8aa28706266
Nikola Iliev <nikola.iliev@sofyma.com>
2018-01-15 10:46:12 +0200

    create number component

--- client/src/app/components/number-component/number-component.component.ts
+++ client/src/app/components/number-component/number-component.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-number-component',
-  templateUrl: './number-component.component.html',
-  styleUrls: ['./number-component.component.css']
+	selector: "app-number-component",
+	templateUrl: "./number-component.component.html",
+	styleUrls: ["./number-component.component.css"]
 })
 export class NumberComponentComponent implements OnInit {
+	value: number = 0;
 
-  constructor() { }
+  	constructor() {}
 
-  ngOnInit() {
-  }
+  	dec() {
+		if (this.value !== 1) {
+			this.value--;
+		}
+		console.log("dec", this.value);	 
+	}
 
+  	inc() {
+		if (this.value !== 100) {
+			this.value++;
+		}
+		console.log("inc", this.value);	 
+	}
+
+  	ngOnInit() {}
 }
