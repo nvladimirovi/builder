1f5862fc380881f72629179e711571d9c07304a2
zenddignite <nikiviliev98@gmail.com>
2018-01-28 14:03:26 +0300

    fix alignment

--- client/src/app/components/alignment/alignment.component.ts
+++ client/src/app/components/alignment/alignment.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { EmailService } from 'app/shared/email/email.service';
+import { DoCheck } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: "app-alignment",
   templateUrl: "./alignment.component.html",
   styleUrls: ["./alignment.component.css"]
 })
-export class AlignmentComponent implements OnInit {
+export class AlignmentComponent implements OnInit, DoCheck {
   private _element: any;
 
   public set element(value: any) {
@@ -20,6 +21,12 @@ export class AlignmentComponent implements OnInit {
   constructor(private emailService: EmailService) {}
 
   ngOnInit() {
+    if (this.emailService.selected_element) {
+      this.element = this.emailService.selected_element[0];
+    }
+  }
+
+  ngDoCheck() {
     if (this.emailService.selected_element) {
       this.element = this.emailService.selected_element[0];
     }
